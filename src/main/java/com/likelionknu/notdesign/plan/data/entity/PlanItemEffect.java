e2684e286f5113724d9b4791b0984c91138102a6
Yun-Hwan Jeon <yunsol267@gmail.com>
2026-08-08 02:00:57 +0900

    refactor: PlanItemEffect name, price 컬럼 추가 (#9)

--- src/main/java/com/likelionknu/notdesign/plan/data/entity/PlanItemEffect.java
+++ src/main/java/com/likelionknu/notdesign/plan/data/entity/PlanItemEffect.java
@@ -16,6 +16,12 @@ public class PlanItemEffect {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private Integer price;
+
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private ImprovementItem improvement;
