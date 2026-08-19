4bc261d6037dcbee05d1ba9ca1d9f0e68b073f24
yunh03 <nanyunan03@gmail.com>
2026-08-19 18:21:46 +0900

    refactor: @deprecated 제거 (#96)

--- src/main/java/com/likelionknu/notdesign/plan/data/enums/ImprovementItem.java
+++ src/main/java/com/likelionknu/notdesign/plan/data/enums/ImprovementItem.java
@@ -10,13 +10,7 @@ import lombok.RequiredArgsConstructor;
 public enum ImprovementItem {
     PIGMENTATION("색소침착"),
     ERYTHEMA("홍조"),
-    PORES("모공"),
-
-    /**
-     * @deprecated 지표 변경으로 인한 삭제 예정
-     */
-    @Deprecated
-    HYDRATION("수분력");
+    PORES("모공");
 
     private final String displayName;
 
